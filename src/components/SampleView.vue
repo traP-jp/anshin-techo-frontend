12161a3e8f4bf99091058d482ef6af844aced590
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2025-12-15 00:49:53 +0900

    Vuetify の導入

--- src/components/SampleView.vue
+++ src/components/SampleView.vue
@@ -1,12 +1,14 @@
 <template>
   <div :class="$style.container">
     <div>Sample View</div>
+    <v-btn text="Hello!" color="blue" prepend-icon="mdi-check" />
   </div>
 </template>
 
 <style module>
 .container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
