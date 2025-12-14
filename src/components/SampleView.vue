c229ccee518a8c5035ea585bb14658ba1997f379
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2025-12-15 00:36:29 +0900

    vue-router の導入

---
+++ src/components/SampleView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.container">
+    <div>Sample View</div>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
