3f8dd833936a7161ae895aafc528ec9b5d6c4ddb
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2025-12-15 00:17:09 +0900

    init

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'eslint/config'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import js from '@eslint/js'
+
+export default defineConfig([
+  { ignores: ['dist'] },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...pluginVue.configs['flat/recommended'],
+
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        project: ['./tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: ['.vue'],
+      },
+    },
+  },
+
+  {
+    rules: {
+      'vue/component-name-in-template-casing': ['warn', 'kebab-case'],
+      'vue/no-template-target-blank': ['error', { enforceDynamicLinks: 'always' }],
+      'vue/no-v-html': 'error',
+    },
+  },
+
+  eslintConfigPrettier, // 常に最後に置く
+])
