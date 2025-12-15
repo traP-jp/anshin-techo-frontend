844ee05d1c714ea9725ebee4804e8d7c675c047f
yomi <chomusuke.1204me@outlook.jp>
2025-12-15 18:37:37 +0900

    cssからvuetifへy記法を変更

--- src/components/TicketNote.vue
+++ src/components/TicketNote.vue
@@ -5,38 +5,14 @@ defineProps<{ note: Note }>()
 </script>
 
 <template>
-  <div :class="$style.container">
+  <div class="d-flex flex-row align-start">
     <user-icon id="Yomi" :size="30" />
-    <div :class="$style.right">
-      <div :class="$style.info">
+    <div class="d-flex flex-column ml-2">
+      <div class="d-flex flex-row ga-2">
         <div>{{ note.author }}</div>
         <div>{{ getDateRepresentation(note.created_at) }}</div>
       </div>
-      <div :class="$style.content">{{ note.content }}</div>
+      <div class="bg-grey-lighten-3 mt-1 px-2 py-3">{{ note.content }}</div>
     </div>
   </div>
 </template>
-
-<style module>
-.container {
-  border: 1px solid blue;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-}
-.right {
-  margin-left: 8px;
-  display: flex;
-  flex-direction: column;
-}
-.info {
-  display: flex;
-  flex-direction: row;
-  gap: 8px;
-}
-.content {
-  margin-top: 4px;
-  background-color: lightgray;
-  padding: 12px 8px;
-}
-</style>
