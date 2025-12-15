ed33cdd1a4be213b9baefb69fd8b23427dafc2ff
yomi <chomusuke.1204me@outlook.jp>
2025-12-15 18:19:21 +0900

    feat: ノートの概形

--- src/components/TicketNote.vue
+++ src/components/TicketNote.vue
@@ -1,13 +1,42 @@
 <script setup lang="ts">
+import UserIcon from './UserIcon.vue'
+import { getDateRepresentation } from '@/utils/date'
 defineProps<{ note: Note }>()
 </script>
 
 <template>
-  <div :class="$style.container">TicketNote {{ note.id }}</div>
+  <div :class="$style.container">
+    <user-icon id="Yomi" :size="30" />
+    <div :class="$style.right">
+      <div :class="$style.info">
+        <div>{{ note.author }}</div>
+        <div>{{ getDateRepresentation(note.created_at) }}</div>
+      </div>
+      <div :class="$style.content">{{ note.content }}</div>
+    </div>
+  </div>
 </template>
 
 <style module>
 .container {
   border: 1px solid blue;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+.right {
+  margin-left: 8px;
+  display: flex;
+  flex-direction: column;
+}
+.info {
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+}
+.content {
+  margin-top: 4px;
+  background-color: lightgray;
+  padding: 12px 8px;
 }
 </style>
---
+++ src/utils/date.ts
@@ -0,0 +1,43 @@
+const getMonthString = (date: Readonly<Date>) => (date.getMonth() + 1).toString().padStart(2, '0')
+
+const getDateString = (date: Readonly<Date>) => date.getDate().toString().padStart(2, '0')
+
+const getHoursString = (date: Readonly<Date>) => date.getHours().toString().padStart(2, '0')
+
+const getMinutesString = (date: Readonly<Date>) => date.getMinutes().toString().padStart(2, '0')
+
+const getTimeString = (date: Readonly<Date>) => `${getHoursString(date)}:${getMinutesString(date)}`
+
+const getDayString = (date: Readonly<Date>) => `${getMonthString(date)}/${getDateString(date)}`
+
+const getFullDayString = (date: Readonly<Date>) => `${date.getFullYear()}/${getDayString(date)}`
+
+export const getDateRepresentation = (date: Readonly<Date> | string) => {
+  const displayDate = new Date(date)
+  if (Number.isNaN(displayDate.getTime())) {
+    return ''
+  }
+  const today = new Date()
+  const timeString = getTimeString(displayDate)
+  const yesterday = new Date(today.getTime() - 1000 * 60 * 60 * 24)
+
+  if (
+    displayDate.getFullYear() === today.getFullYear() &&
+    displayDate.getMonth() === today.getMonth() &&
+    displayDate.getDate() === today.getDate()
+  ) {
+    return `今日 ${timeString}`
+  }
+  if (
+    displayDate.getFullYear() === yesterday.getFullYear() &&
+    displayDate.getMonth() === yesterday.getMonth() &&
+    displayDate.getDate() === yesterday.getDate()
+  ) {
+    return `昨日 ${timeString}`
+  }
+  if (displayDate.getFullYear() === today.getFullYear()) {
+    return `${getDayString(displayDate)} ${timeString}`
+  } else {
+    return `${getFullDayString(displayDate)} ${timeString}`
+  }
+}
