bc56d89779299b854a599338936f8ece1bb4a23a
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2025-12-15 20:44:47 +0900

    feat: fontsource からフォントを導入

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import router from '@/router'
 import vuetify from '@/plugins/vuetify'
 import { createPinia } from 'pinia'
 
+import '@fontsource-variable/reddit-sans' // 'Reddit Sans Variable'
+import '@fontsource-variable/roboto-mono' // 'Roboto Mono Variable'
+import '@fontsource/noto-sans-jp/400.css' // 'Noto Sans JP'
+import '@fontsource/noto-sans-jp/700.css'
+
 const pinia = createPinia()
 
 const app = createApp(App)
