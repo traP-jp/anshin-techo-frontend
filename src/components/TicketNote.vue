d03332425153064bf48ced985272adfc3a1dcf55
yomi <chomusuke.1204me@outlook.jp>
2025-12-15 18:50:53 +0900

    名前太字、ボーダー作った

--- src/components/TicketNote.vue
+++ src/components/TicketNote.vue
@@ -9,10 +9,22 @@ defineProps<{ note: Note }>()
     <user-icon id="Yomi" :size="30" />
     <div class="d-flex flex-column ml-2">
       <div class="d-flex flex-row ga-2">
-        <div>{{ note.author }}</div>
+        <div class="font-weight-bold">{{ note.author }}</div>
+        <div :class="$style.border"></div>
         <div>{{ getDateRepresentation(note.created_at) }}</div>
       </div>
-      <div class="bg-grey-lighten-3 mt-1 px-2 py-3">{{ note.content }}</div>
+      <div class="bg-grey-lighten-3 mt-1 px-2 py-3" :class="$style.content">{{ note.content }}</div>
     </div>
   </div>
 </template>
+
+<style module>
+.content {
+  border-radius: 0px 8px 8px 8px;
+}
+.border {
+  width: 2px;
+  margin: 4px 0px;
+  background-color: black;
+}
+</style>
