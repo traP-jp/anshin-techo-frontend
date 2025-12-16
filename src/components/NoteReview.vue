323c0eaf588afc94720d90fd2d740dd614367d85
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2025-12-16 16:02:42 +0900

    feat: 基本形のデザイン実装

--- src/components/NoteReview.vue
+++ src/components/NoteReview.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
+import UserIcon from './UserIcon.vue'
+import { getDateRepresentation } from '@/utils/date'
 defineProps<{ review: Review }>()
 </script>
 
 <template>
-  <div :class="$style.container">NoteReview {{ review.id }}</div>
+  <div class="flex-grow-1 d-flex flex-row ga-2 align-start ma-3">
+    <user-icon :id="review.reviewer" :size="40" />
+
+    <v-card variant="outlined" color="grey-lighten-1" class="flex-grow-1 text-body-1 text-black">
+      <v-card-title class="pa-0 bg-grey-lighten-1 text-white d-flex flex-row align-center">
+        <div class="text-body-2 ml-1 pa-2 font-weight-medium">{{ review.reviewer }}</div>
+        <div :class="$style.date">
+          {{ getDateRepresentation(review.created_at) }}
+        </div>
+      </v-card-title>
+      <v-card-text class="text-black pa-3">{{ review.comment }}</v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <style module>
 .container {
-  border: 1px solid red;
+  border: 1px solid rgb(var(--v-theme-grey-lighten-2));
+  border-radius: 8px;
+}
+
+.date {
+  font-family: 'Inter Variable';
+  margin-top: 2px;
+  font-size: 12px;
+  font-weight: 500;
 }
 </style>
