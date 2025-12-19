248d1b847a9204de322e6ef46c2a46601e80d929
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2025-12-19 13:57:02 +0900

    伏字エディタをコンポーネント化

---
+++ src/components/shared/SpoilerEditorWrapper.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import SpoilerEditor from '@/components/shared/SpoilerEditor.vue'
+
+const isFocused = ref(false)
+const content = defineModel<string>() // 親からは v-model で受け取る
+
+const onFocusChange = (focused: boolean) => {
+  isFocused.value = focused
+}
+
+const onEdit = (newContent: string) => {
+  content.value = newContent
+}
+</script>
+
+<template>
+  <div :class="[$style.input, { [$style.focused]: isFocused }]">
+    <spoiler-editor
+      :initial-content="decodeURI($route.path)"
+      @focus="onFocusChange"
+      @edit="onEdit"
+    />
+  </div>
+</template>
+
+<style module>
+.input {
+  padding: 4px;
+  border-radius: 3px;
+  outline: 1px solid rgb(var(--v-theme-border));
+  transition: outline-color 0.1s;
+}
+
+.focused {
+  outline: 1.5px solid rgb(var(--v-theme-input));
+}
+</style>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,54 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
-import SpoilerEditor from '@/components/shared/SpoilerEditor.vue'
+import SpoilerEditorWrapper from '@/components/shared/SpoilerEditorWrapper.vue'
 import SpoilerViewer from '@/components/shared/SpoilerViewer.vue'
 
 const route = useRoute()
-
-const isFocused = ref(false)
 const content = ref(decodeURI(route.path))
-
-const onFocusChange = (focused: boolean) => {
-  isFocused.value = focused
-}
-
-const onEdit = (newContent: string) => {
-  content.value = newContent
-}
 </script>
 
 <template>
   <div class="ma-4 d-flex flex-column ga-4">
-    <spoiler-viewer :text="content" class="mt-4" />
-    <div class="pa-1 rounded" :class="[$style.input, { [$style.focused]: isFocused }]">
-      <spoiler-editor
-        :initial-content="decodeURI($route.path)"
-        @focus="onFocusChange"
-        @edit="onEdit"
-      />
-    </div>
+    <spoiler-viewer :text="content" class="mt-4" visible />
+    <spoiler-editor-wrapper v-model="content" />
     <v-textarea variant="outlined" rows="1" auto-grow density="compact" />
   </div>
 </template>
-
-<style module>
-.input {
-  box-sizing: border-box;
-  border-radius: 4px;
-  outline: 1px solid #c0c0c0;
-  transition: outline-color 0.2s;
-}
-
-.focused {
-  outline: 1.5px solid rgba(76, 0, 255);
-}
-
-.path {
-  padding: 0px 20px;
-  font-size: 24px;
-  font-family: monospace;
-  border-radius: 6px;
-  border: 1px solid black;
-}
-</style>
