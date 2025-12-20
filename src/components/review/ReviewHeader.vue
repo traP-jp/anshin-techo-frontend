3a36fb87eaf24e3ac30df3fd5a6d5a172487d930
yomi <chomusuke.1204me@outlook.jp>
2025-12-20 14:37:47 +0900

    feat: サイドバー見た目だけ

--- src/components/review/ReviewHeader.vue
+++ src/components/review/ReviewHeader.vue
@@ -4,27 +4,45 @@ import SpoilerViewer from '@/components/shared/SpoilerViewer.vue'
 import { getDateRepresentation } from '@/utils/date'
 
 defineProps<{ note: Note }>()
+
+const approvedCount = 3
+const requiredCount = 5
+const progress = (approvedCount / requiredCount) * 100
 </script>
 
 <template>
-  <div class="d-flex flex-row align-start">
-    <!-- 投稿者アイコン -->
-    <user-icon :id="note.author" :size="36" :external="note.type === 'incoming'" />
+  <div class="d-flex flex-column">
+    <div class="d-flex flex-row align-start">
+      <!-- 投稿者アイコン -->
+      <user-icon :id="note.author" :size="36" :external="note.type === 'incoming'" />
 
-    <div class="d-flex flex-column ml-2">
-      <!-- 投稿者名・日時 -->
-      <div class="d-flex align-center ga-2">
-        <div class="font-weight-bold text-high-emphasis">{{ note.author }}</div>
-        <div class="bg-grey" :class="$style.border"></div>
-        <div class="text-medium-emphasis text-body-2">
-          {{ getDateRepresentation(note.created_at) }}
+      <div class="d-flex flex-column ml-2">
+        <!-- 投稿者名・日時 -->
+        <div class="d-flex align-center ga-2">
+          <div class="font-weight-bold text-high-emphasis">{{ note.author }}</div>
+          <div class="bg-grey" :class="$style.border"></div>
+          <div class="text-medium-emphasis text-body-2">
+            {{ getDateRepresentation(note.created_at) }}
+          </div>
+        </div>
+
+        <!-- ノート本文 -->
+        <div class="mt-1 text-body-2 ps-2 pe-3 border">
+          <spoiler-viewer :text="note.content" />
         </div>
       </div>
-
-      <!-- ノート本文 -->
-      <div class="mt-1 text-body-2 ps-2 pe-3">
-        <spoiler-viewer :text="note.content" />
-      </div>
+    </div>
+    <div class="d-flex flex-column border">
+      <p class="text-body-2 text-grey-darken-2 mb-2">承認</p>
+      <v-progress-linear
+        :model-value="progress"
+        chunk-count="5"
+        chunk-gap="10"
+        color="green"
+        height="25px"
+      >
+        <small class="text-white">{{ approvedCount }}/{{ requiredCount }}</small>
+      </v-progress-linear>
     </div>
   </div>
 </template>
