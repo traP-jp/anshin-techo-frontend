21b2448a923bc6231fc0583b621f5a22197dca94
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2025-12-21 12:06:23 +0900

    ユーザーの追加はできていそう

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
+import { api } from '@/api'
+import { onMounted } from 'vue'
 import ThemeToggle from './components/shared/ThemeToggle.vue'
+onMounted(async () => {
+  console.log(await api.getUsers())
+  console.log(
+    await api.putUsers([
+      {
+        traq_id: 'kitsne',
+        role: 'manager',
+      },
+    ])
+  )
+
+  console.log(
+    await api.postTicket({
+      title: '初回セットアップ用チケット',
+      description: 'このチケットはアプリケーションの初回セットアップのために自動生成されました。',
+      status: 'sent',
+      assignee: 'kitsne',
+      sub_assignees: [],
+      stakeholders: [],
+      due: '2025-12-31',
+      tags: ['setup'],
+    })
+  )
+})
 </script>
 
 <template>
--- src/api.ts
+++ src/api.ts
@@ -3,111 +3,133 @@ import { useUserStore } from './store'
 
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
 
-// あたたかみのあるてがき
-export const fetchApi = async (
-  method: HttpMethod,
-  path: string,
-  option?: { queryParams?: Record<string, string>; body?: Record<string, any> }
-) => {
-  const { userId } = useUserStore()
-  const queryParamStr = option?.queryParams
-    ? '?' + new URLSearchParams(option?.queryParams).toString()
-    : ''
+const apiClient = () => {
+  // あたたかみのあるてがき
+  const fetchApi = async (
+    method: HttpMethod,
+    path: string,
+    option?: { queryParams?: Record<string, string>; body?: Record<string, any> }
+  ) => {
+    const { userId } = useUserStore()
+    const queryParamStr = option?.queryParams
+      ? '?' + new URLSearchParams(option?.queryParams).toString()
+      : ''
 
-  const request: RequestInit = {
-    method: method,
-    headers: { 'X-Forwarded-User': userId!, 'Content-Type': 'application/json' },
-    body: option?.body ? JSON.stringify(option.body) : undefined,
+    const request: RequestInit = {
+      method: method,
+      headers: { 'X-Forwarded-User': userId!, 'Content-Type': 'application/json' },
+      body: option?.body ? JSON.stringify(option.body) : undefined,
+    }
+
+    const res = await fetch(`/api${path}${queryParamStr}`, request)
+    return await res.json()
   }
 
-  const res = await fetch(`/api${path}${queryParamStr}`, request)
-  return res.status === 200 ? await res.json() : { error: await res.text() }
-}
+  // --- Tickets ---
 
-// --- Tickets ---
-
-export const getTickets = async (assignee: string, status: Ticket['status'], sort: string) => {
-  return fetchApi('GET', '/tickets', { queryParams: { assignee, status, sort } })
-}
-
-export const postTickets = async (body: PostTicket) => {
-  return fetchApi('POST', '/tickets', { body })
-}
-
-export const getTicket = async (ticketId: number) => {
-  return fetchApi('GET', `/tickets/${ticketId}`)
-}
-
-export const patchTicket = async (ticketId: number, body: PostTicket) => {
-  return fetchApi('PATCH', `/tickets/${ticketId}`, { body })
-}
-
-export const deleteTicket = async (ticketId: number) => {
-  return fetchApi('DELETE', `/tickets/${ticketId}`)
-}
-
-// --- Notes ---
-
-export const postNote = async (
-  ticketId: number,
-  body: { type: Note['type']; content: string; mention_notification: boolean }
-) => {
-  return fetchApi('POST', `/tickets/${ticketId}/notes`, { body })
-}
-
-export const putNote = async (
-  ticketId: number,
-  noteId: number,
-  body: { content?: string; status?: Note['status']; reset_reviews?: boolean }
-) => {
-  return fetchApi('PUT', `/tickets/${ticketId}/notes/${noteId}`, { body })
-}
-
-export const deleteNote = async (ticketId: number, noteId: number) => {
-  return fetchApi('DELETE', `/tickets/${ticketId}/notes/${noteId}`)
-}
-
-// --- Reviews ---
-
-export const postReview = async (ticketId: number, noteId: number, body: PostReview) => {
-  return fetchApi('POST', `/tickets/${ticketId}/notes/${noteId}/reviews`, { body })
-}
-
-export const putReview = async (
-  ticketId: number,
-  noteId: number,
-  reviewId: number,
-  body: PostReview
-) => {
-  return fetchApi('PUT', `/tickets/${ticketId}/notes/${noteId}/reviews/${reviewId}`, { body })
-}
-
-export const deleteReview = async (ticketId: number, noteId: number, reviewId: number) => {
-  return fetchApi('DELETE', `/tickets/${ticketId}/notes/${noteId}/reviews/${reviewId}`)
-}
-
-// --- Users ---
-
-export const getUsers = async () => {
-  return fetchApi('GET', '/users')
-}
-
-export const putUsers = async (body: User[]) => {
-  return fetchApi('PUT', '/users', { body })
-}
-
-// --- Config ---
-
-export const getConfig = async () => {
-  return fetchApi('GET', '/config')
-}
-
-export const postConfig = async (body: {
-  reminder_interval: {
-    overdue_day: number[]
-    notesent_hour: number
+  const getTickets = async (assignee: string, status: Ticket['status'], sort: string) => {
+    return fetchApi('GET', '/tickets', { queryParams: { assignee, status, sort } })
   }
-  revise_prompt: string
-}) => {
-  return fetchApi('POST', '/config', { body })
+
+  const postTicket = async (body: PostTicket) => {
+    return fetchApi('POST', '/tickets', { body })
+  }
+
+  const getTicket = async (ticketId: number) => {
+    return fetchApi('GET', `/tickets/${ticketId}`)
+  }
+
+  const patchTicket = async (ticketId: number, body: PostTicket) => {
+    return fetchApi('PATCH', `/tickets/${ticketId}`, { body })
+  }
+
+  const deleteTicket = async (ticketId: number) => {
+    return fetchApi('DELETE', `/tickets/${ticketId}`)
+  }
+
+  // --- Notes ---
+
+  const postNote = async (
+    ticketId: number,
+    body: { type: Note['type']; content: string; mention_notification: boolean }
+  ) => {
+    return fetchApi('POST', `/tickets/${ticketId}/notes`, { body })
+  }
+
+  const putNote = async (
+    ticketId: number,
+    noteId: number,
+    body: { content?: string; status?: Note['status']; reset_reviews?: boolean }
+  ) => {
+    return fetchApi('PUT', `/tickets/${ticketId}/notes/${noteId}`, { body })
+  }
+
+  const deleteNote = async (ticketId: number, noteId: number) => {
+    return fetchApi('DELETE', `/tickets/${ticketId}/notes/${noteId}`)
+  }
+
+  // --- Reviews ---
+
+  const postReview = async (ticketId: number, noteId: number, body: PostReview) => {
+    return fetchApi('POST', `/tickets/${ticketId}/notes/${noteId}/reviews`, { body })
+  }
+
+  const putReview = async (
+    ticketId: number,
+    noteId: number,
+    reviewId: number,
+    body: PostReview
+  ) => {
+    return fetchApi('PUT', `/tickets/${ticketId}/notes/${noteId}/reviews/${reviewId}`, { body })
+  }
+
+  const deleteReview = async (ticketId: number, noteId: number, reviewId: number) => {
+    return fetchApi('DELETE', `/tickets/${ticketId}/notes/${noteId}/reviews/${reviewId}`)
+  }
+
+  // --- Users ---
+
+  const getUsers = async () => {
+    return fetchApi('GET', '/users')
+  }
+
+  const putUsers = async (body: User[]) => {
+    return fetchApi('PUT', '/users', { body })
+  }
+
+  // --- Config ---
+
+  const getConfig = async () => {
+    return fetchApi('GET', '/config')
+  }
+
+  const postConfig = async (body: {
+    reminder_interval: {
+      overdue_day: number[]
+      notesent_hour: number
+    }
+    revise_prompt: string
+  }) => {
+    return fetchApi('POST', '/config', { body })
+  }
+
+  return {
+    getTickets,
+    postTicket,
+    getTicket,
+    patchTicket,
+    deleteTicket,
+    postNote,
+    putNote,
+    deleteNote,
+    postReview,
+    putReview,
+    deleteReview,
+    getUsers,
+    putUsers,
+    getConfig,
+    postConfig,
+  }
 }
+
+export const api = apiClient()
